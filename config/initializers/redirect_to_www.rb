278e9e6141f272ab0b0f7b98053e626119ee3d88
Matsushita Masahiro <matsushitamasahiro@example.com>
2025-11-12 23:18:08 +0900

    Fix Rack::HostRedirect require path

--- config/initializers/redirect_to_www.rb
+++ config/initializers/redirect_to_www.rb
@@ -1,3 +1,5 @@
 # wwwなしのドメインからwwwありのドメインへリダイレクト
+require_relative '../../lib/rack/host_redirect'
+
 Rails.application.config.middleware.insert_before 0, Rack::HostRedirect,
   'hirokuru.com' => 'www.hirokuru.com'
